ec03f033f5bb65a481c18d66793cfc40ca23c570
lcf <1305710704@qq.com>
2019-03-01 13:05:25 +0800

    基础类的添加 LCF

---
+++ src/main/java/com/incubationplatform/common/Const.java
@@ -0,0 +1,9 @@
+package com.incubationplatform.common;
+
+/**
+ * @author liaochaofan
+ * @date 2019/3/1 11:01
+ */
+public class Const {
+
+}
---
+++ src/main/java/com/incubationplatform/common/ServerResponse.java
@@ -0,0 +1,69 @@
+package com.incubationplatform.common;
+
+/**
+ * @author liaochaofan
+ * @date 2019/3/1 11:04
+ */
+public class ServerResponse<T> {
+    private int net;
+    private String msg;
+    private T data;
+    private ServerResponse(int net){
+        this.net = net;
+    }
+    private ServerResponse(int net, T data){
+        this.net = net;
+        this.data = data;
+    }
+
+    public ServerResponse(int net, String msg, T data) {
+        this.net = net;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public ServerResponse(int net, String msg) {
+        this.net = net;
+        this.msg = msg;
+    }
+
+    public boolean isSuccess(){
+        return this.net == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getNet() {
+        return net;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
+    public static <T> ServerResponse<T> createBySuccess(String msg,T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+    }
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
+        return new ServerResponse<T>(errorCode,errorMessage);
+    }
+
+}
