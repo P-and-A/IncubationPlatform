4224e50ad352bf08529530cc0b13ae02c6c31781
lcf <1305710704@qq.com>
2019-03-06 08:38:03 +0800

    常量类补充项目状态常量 LCF

--- src/main/java/com/incubationplatform/common/Const.java
+++ src/main/java/com/incubationplatform/common/Const.java
@@ -5,6 +5,30 @@ package com.incubationplatform.common;
  * @date 2019/3/1 11:01
  */
 public class Const {
+
+    public enum ProjectStatus{
+        CollegePass(10,"学院审核通过"),
+        CollegeNoPass(11,"学院审核未通过"),
+        SchoolPass(20,"学校审核通过"),
+        SchoolNoPass(21,"学校审核未通过");
+        //审核状态码
+        private int code;
+        //审核状态
+        private String status;
+
+        ProjectStatus(int code, String status) {
+            this.code = code;
+            this.status = status;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+    }
     public interface MessageClassification{
         String ANNOUNCEMENTS = "通知公告";
     }
