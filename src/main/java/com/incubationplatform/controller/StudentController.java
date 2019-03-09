43e6002e384cd6e920d10b188d113f845e37d005
gmuk <1427600836@qq.com>
2019-03-09 22:00:03 +0800

    增加学生端个人信息的查看和修改

--- src/main/java/com/incubationplatform/controller/StudentController.java
+++ src/main/java/com/incubationplatform/controller/StudentController.java
@@ -1,6 +1,12 @@
 package com.incubationplatform.controller;
 
 
+import com.incubationplatform.common.ServerResponse;
+import com.incubationplatform.pojo.Student;
+import com.incubationplatform.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -16,6 +22,29 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    IStudentService iStudentService;
 
+    @RequestMapping("/person")
+    public ServerResponse getUserInfo(){
+        Student user = (Student) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        ServerResponse serverResponse=iStudentService.getStudentById(user.getId());
+        return serverResponse;
+    }
+
+    @RequestMapping("/person/submit")
+    public ServerResponse updateUserInfo(Student student){
+        Student user = (Student) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        student.setId(user.getId());
+        iStudentService.updateStudentInfo(student);
+        return null;
+    }
+
+    @RequestMapping("/student/change_password")
+    public ServerResponse updateUserPassword(String newPassword,String oldPassword){
+        Student user = (Student) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        iStudentService.updatePassword(user,newPassword,oldPassword);
+        return null;
+    }
 }
 
--- src/main/java/com/incubationplatform/service/impl/StudentServiceImpl.java
+++ src/main/java/com/incubationplatform/service/impl/StudentServiceImpl.java
@@ -1,10 +1,13 @@
 package com.incubationplatform.service.impl;
 
+import com.incubationplatform.common.ServerResponse;
 import com.incubationplatform.pojo.Student;
 import com.incubationplatform.dao.StudentDao;
 import com.incubationplatform.service.IStudentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 /**
  * <p>
@@ -16,5 +19,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StudentServiceImpl extends ServiceImpl<StudentDao, Student> implements IStudentService {
+    @Autowired
+    StudentDao studentDao;
 
+    //根据用户id获取用户信息
+    @Override
+    public ServerResponse getStudentById(String id) {
+        Student student = studentDao.selectById(id);
+        student.setPassword(null);
+        return ServerResponse.createBySuccess(student);
+    }
+
+    //根据当前登录的用户修改密码
+    @Override
+    public ServerResponse updatePassword(Student student, String newPassword, String oldPassword) {
+        Student user = studentDao.selectById(student.getId());
+        DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        if (user.getPassword().equals(DigestUtils.md5DigestAsHex(oldPassword.getBytes()))) {
+            student.setPassword(DigestUtils.md5DigestAsHex(newPassword.getBytes()));
+            if (studentDao.updateById(student) > 0) {
+                return ServerResponse.createBySuccess();
+            } else {
+                return ServerResponse.createByErrorMessage("修改失败，未知错误");
+            }
+        } else {
+            return ServerResponse.createByErrorMessage("原密码不一致");
+        }
+    }
+
+    //根据当前登录的用户修改用户信息
+    @Override
+    public ServerResponse updateStudentInfo(Student student) {
+        if (studentDao.updateById(student)>0){
+            return ServerResponse.createBySuccess();
+        }else {
+            return ServerResponse.createByErrorMessage("修改失败，未知错误");
+        }
+    }
 }
