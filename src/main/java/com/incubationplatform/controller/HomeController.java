548d13db8d0445293cd78658ca76b20bcccd6d78
lcf <1305710704@qq.com>
2019-03-11 20:51:33 +0800

    网站首页，网站首页-立项项目栏，网站首页-新闻动态的接口完成 LCF

--- src/main/java/com/incubationplatform/controller/HomeController.java
+++ src/main/java/com/incubationplatform/controller/HomeController.java
@@ -1,11 +1,16 @@
 package com.incubationplatform.controller;
 
+import com.incubationplatform.common.Const;
 import com.incubationplatform.common.ServerResponse;
 import com.incubationplatform.service.IHomeService;
+import com.incubationplatform.service.IMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author liaochaofan
@@ -18,10 +23,27 @@ public class HomeController {
     @Autowired
     private IHomeService iHomeService;
 
+    @Autowired
+    private IMessageService iMessageService;
+
+    @RequestMapping("")
+    @ResponseBody
+    public ServerResponse home(){
+        return iHomeService.getHomeMessage();
+    }
+
     @RequestMapping("/message")
     @ResponseBody
     public ServerResponse getMessage(){
         return iHomeService.getMessage();
+    }
+
+    @RequestMapping("/new")
+    @ResponseBody
+    public ServerResponse getNew(){
+        Map<String,Object> messageCondition = new HashMap<>();
+        messageCondition.put("classification", Const.MessageClassification.NEW);
+        return ServerResponse.createBySuccess(iMessageService.listByMap(messageCondition));
     }
 
     @RequestMapping("/annual_meeting")
--- src/main/java/com/incubationplatform/service/impl/HomeServiceImpl.java
+++ src/main/java/com/incubationplatform/service/impl/HomeServiceImpl.java
@@ -2,13 +2,15 @@ package com.incubationplatform.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.incubationplatform.common.Const;
-import com.incubationplatform.common.ResponseCode;
 import com.incubationplatform.common.ServerResponse;
+import com.incubationplatform.dao.LinkDao;
 import com.incubationplatform.dao.MessageDao;
 import com.incubationplatform.dao.MessageVideoDao;
+import com.incubationplatform.dao.ProjectDao;
 import com.incubationplatform.pojo.Link;
 import com.incubationplatform.pojo.Message;
 import com.incubationplatform.pojo.MessageVideo;
+import com.incubationplatform.pojo.Project;
 import com.incubationplatform.service.IHomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,33 @@ public class HomeServiceImpl implements IHomeService {
     private MessageDao messageDao;
     @Autowired
     private MessageVideoDao messageVideoDao;
+    @Autowired
+    private ProjectDao projectDao;
+    @Autowired
+    private LinkDao linkDao;
+
+    public ServerResponse getHomeMessage(){
+        Map<String,Object> resultMap = new HashMap<>();
+        List<Link> linkList = linkDao.selectByMap(null);
+        Map<String,Object> conditionMap = new HashMap<>();
+        conditionMap.put("classification",Const.ProjectClassification.EXCELLENT_INNOVATE);
+        List<Project> excellentInnovates = projectDao.selectByMap(conditionMap);
+        conditionMap.put("classification",Const.ProjectClassification.EXCELLENT_VENTURE);
+        List<Project> excellentVentures = projectDao.selectByMap(conditionMap);
+        conditionMap.put("classification",Const.ProjectClassification.EXCELLENT_PRACTICE);
+        List<Project> excellentPractices = projectDao.selectByMap(conditionMap);
+
+        List<Message> characterStyleList = messageDao.selectList(new QueryWrapper<Message>()
+                .orderByDesc("create_time"));
+        resultMap.put("link",linkList);
+        resultMap.put("excellentInnovate",excellentInnovates);
+        resultMap.put("excellentVenture",excellentVentures);
+        resultMap.put("excellentPractice",excellentPractices);
+        resultMap.put("talent",characterStyleList);
+        return ServerResponse.createBySuccess(resultMap);
+    }
+
+
     public ServerResponse<List<Message>> getMessage(){
         Map<String,Object> columMap = new HashMap<>();
         columMap.put("classification", Const.MessageClassification.ANNOUNCEMENTS);
