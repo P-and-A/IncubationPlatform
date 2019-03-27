57bf2d3e91e3d1b00f98715942965525902d303d
gmuk <1427600836@qq.com>
2019-03-28 07:47:06 +0800

    首页整合

--- src/main/java/com/incubationplatform/controller/StudentController.java
+++ src/main/java/com/incubationplatform/controller/StudentController.java
@@ -6,11 +6,9 @@ import com.incubationplatform.pojo.Student;
 import com.incubationplatform.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * <p>
